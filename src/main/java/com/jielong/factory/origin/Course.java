3f7f91ce63474bd977974310637914644b873d80
zhao750456695 <zhao750456695@163.com>
2021-09-19 08:54:00 +0800

    simple factory

---
+++ src/main/java/com/jielong/factory/origin/Course.java
@@ -0,0 +1,19 @@
+/**
+ * projectName: design-pattern
+ * fileName: Course.java
+ * packageName: com.jielong.factory.origin
+ * date: 2021-09-19 8:53
+ * copyright(c) 2017-2020 xxx公司
+ */
+package com.jielong.factory.origin;
+
+/**
+ * @version: V1.0
+ * @author: JieZhao
+ * @className: Course
+ * @packageName: com.jielong.factory.origin
+ * @description:
+ * @data: 2021-09-19 8:53
+ **/
+public class Course {
+}
